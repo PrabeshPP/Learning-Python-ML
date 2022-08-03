cbe99699e61760313e5c1ba0dd3942b27d142876
Prabesh <prabesh.bista369@gmail.com>
2022-08-03 20:05:02 +0545

    [completed] python modules

--- day1/modules.py
+++ day1/modules.py
@@ -1,4 +1,10 @@
 import calendar
-cal=calendar.month(2019,7)
+import math
+import random
 
+list=['a','b','c','d','e','f']
+cal=calendar.month(2019,7)
+root=math.sqrt(169)
+print(root)
 print(cal)
+print(list)
